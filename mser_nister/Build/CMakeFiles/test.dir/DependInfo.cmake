b29a9597754993f61f99e3cb738dd92335ebae26
alexhagiopol <alexhagiopol@gmail.com>
2015-02-19 11:09:48 -0500

    Added Nister MSER implementation.

---
+++ mser_nister/Build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/alexhagiopol/Documents/Laboratory/Project_MSER/mser_nister/mser.cpp" "/Users/alexhagiopol/Documents/Laboratory/Project_MSER/mser_nister/Build/CMakeFiles/test.dir/mser.cpp.o"
+  "/Users/alexhagiopol/Documents/Laboratory/Project_MSER/mser_nister/test.cpp" "/Users/alexhagiopol/Documents/Laboratory/Project_MSER/mser_nister/Build/CMakeFiles/test.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
