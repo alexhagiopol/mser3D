e12bdd0a04d768e78fc4f0573ea8173e2c0ef51e
Alex Hagiopol <alexhagiopol@gmail.com>
2016-03-12 22:49:33 -0500

    Small cleanup after moving to new machine.

--- mser_3d/main.cpp
+++ mser_3d/main.cpp
@@ -2,7 +2,7 @@
 #include "InputManager.h"
 
 int main(int argc, char ** argv) {
-    assert(argc == 2);
+    assert(argc == 2 && "Please provide the file path of your settings file e.g. /home/you/mser/Settings.yaml");
     std::string settingsFileLocation = argv[1];
     InputManager myInput(settingsFileLocation);
     test3DReconstruction(myInput);
